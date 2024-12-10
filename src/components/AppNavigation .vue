69021d77b4611247d556cdc7be5771f015f3d5bf
AnteAntonini <anteantonini.mtk@hotmail.com>
2024-12-10 20:23:26 +0100

    Remove unnecessary packages Add icons, search input and styling for Navigation

--- src/components/AppNavigation .vue
+++ src/components/AppNavigation .vue
@@ -1,18 +1,35 @@
-<script setup></script>
+<script setup>
+import SearchComponent from './SearchComponent.vue'
+</script>
 
 <template>
   <header class="header">
     <nav class="nav">
       <div class="logo">
         <img src="../assets/logo.svg" />
-        <span>LOGO MyApp</span>
+      </div>
+      <div class="left-nav">
+        <ul class="nav-links">
+          <li>
+            <RouterLink to="/women">Women</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/Men">Men</RouterLink>
+          </li>
+        </ul>
+
+        <div class="search"><SearchComponent /></div>
       </div>
       <ul class="nav-links">
         <li>
-          <RouterLink to="/">Home</RouterLink>
+          <img src="../assets/icons/profile-icon.svg" />
         </li>
-        <RouterLink to="/about">About</RouterLink>
-        <li></li>
+        <li>
+          <img src="../assets/icons/favorite-icon.svg" />
+        </li>
+        <li>
+          <img src="../assets/icons/shopping-cart-icon.svg" />
+        </li>
       </ul>
     </nav>
   </header>
@@ -20,14 +37,16 @@
 
 <style scoped>
 .header {
-  background-color: #f8f9fa;
+  background-color: #2d2d2d;
   padding: 1rem 2rem;
-  border-bottom: 1px solid #eaeaea;
+  border-bottom: 1px solid #2d2d2d;
 }
 
 .nav {
   display: flex;
+  justify-content: center;
   align-items: center;
+  flex-wrap: nowrap;
 }
 
 .logo {
@@ -41,17 +60,28 @@
   width: 40px;
 }
 
+.left-nav {
+  display: flex;
+  width: 100%;
+}
+
 .nav-links {
   list-style: none;
   display: flex;
+  align-items: center;
   gap: 1.5rem;
   padding: 0;
   margin: 0;
 }
 
+.search {
+  width: 100%;
+  padding: 0 1rem;
+}
+
 .nav-links a {
   text-decoration: none;
-  color: #333;
+  color: white;
   font-weight: bold;
   transition: color 0.3s ease;
 }
---
+++ src/components/SearchComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <input class="search" type="text" placeholder="Search for items and brands" />
+</template>
+
+<style scoped>
+.search {
+  font-size: 1rem;
+  width: 100%;
+  height: 38px;
+  border: 0;
+  border-radius: 19px/50%;
+}
+</style>
